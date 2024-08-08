5caf574e323652b365e0d132e9c725c38d36eae1
Akshata Satpute <140546785+akshatasatpute@users.noreply.github.com>
2024-08-08 13:34:12 +0530

    Update Home.py

--- scripts/Home.py
+++ scripts/Home.py
@@ -58,7 +58,8 @@ st.write("Selected Cohort:", selected_Cohort)
 github_urls = {
     "Goal Setting": 'https://docs.google.com/spreadsheets/d/1N5xI5KkCxKL6YYA2a3mfaj4F-wSl5zFifu4A84UHoOY/edit?usp=sharing',
     "Resume": 'https://docs.google.com/spreadsheets/d/1kMc2nZLZUTHID6pHLl3FFeYyPNoZHN3aFaPpxvmBoI8/edit?usp=sharing',
-    "SMART Goals": 'https://docs.google.com/spreadsheets/d/1D7apWcACHvNFvj0Ch5y7jAhpxWCCSoV9xvxFeWqziY8/edit?usp=sharing'
+    "SMART Goals": 'https://docs.google.com/spreadsheets/d/1D7apWcACHvNFvj0Ch5y7jAhpxWCCSoV9xvxFeWqziY8/edit?usp=sharing',
+    "LinkedIn": "https://docs.google.com/spreadsheets/d/14K50qBsKqVmTTNw8UwAXIoEMS69pIPai2_6uXDyc6oE/edit?usp=sharing"
 
     # Add more assignments as needed
 }
