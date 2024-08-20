6411b9f43dc1046993822414b340f23604203263
Akshata Satpute <140546785+akshatasatpute@users.noreply.github.com>
2024-08-20 11:00:13 +0530

    Update Home.py

--- scripts/Home.py
+++ scripts/Home.py
@@ -57,10 +57,10 @@ st.write("Selected Cohort:", selected_Cohort)
 # Define the GitHub API URLs for the folders containing CSV files
 github_urls = {
     "Goal Setting": 'https://docs.google.com/spreadsheets/d/1N5xI5KkCxKL6YYA2a3mfaj4F-wSl5zFifu4A84UHoOY/edit?usp=sharing',
-    "Resume": 'https://docs.google.com/spreadsheets/d/1kMc2nZLZUTHID6pHLl3FFeYyPNoZHN3aFaPpxvmBoI8/edit?usp=sharing',
-    "SMART Goals": 'https://docs.google.com/spreadsheets/d/1D7apWcACHvNFvj0Ch5y7jAhpxWCCSoV9xvxFeWqziY8/edit?usp=sharing',
-    "Searching & Securing Internship": 'https://docs.google.com/spreadsheets/d/1KSvcIF33jscpnboO65KIMt41uqPceGB2L4Ghrdj75zk/edit?usp=sharing',
-    "Career Exploration": 'https://docs.google.com/spreadsheets/d/1bqR2RUHLKyyDF0pY1QbvN65ycZ2CfCrIYyWzc9tO_gg/edit?usp=sharing'
+    "Resume": 'https://docs.google.com/spreadsheets/d/159uBGYMsbJGv70jbXF5vwEdB7m8zbubif_EbY1TFeVg/edit?usp=sharing',
+    "SMART Goals": 'https://docs.google.com/spreadsheets/d/19UrJUqaeXPDVcL6_bN0CiIc3fcSh_DputLNl4ciiR3w/edit?usp=sharing',
+    "Searching & Securing Internship": 'https://docs.google.com/spreadsheets/d/1DnUPMOOxqgaPRJ6b0V9e8usGQtkVG1qJ7ffRzl_E5Uc/edit?usp=sharing',
+    "Career Exploration": 'https://docs.google.com/spreadsheets/d/1nKOWNzHazM49Dw15vMEWhdJEhSTZvk3Wv4DU-eaImHY/edit?usp=sharing'
 
     # Add more assignments as needed
 }
