641993f7d9408979416209cd069e2388acb4037d
Akshata Satpute <140546785+akshatasatpute@users.noreply.github.com>
2024-09-12 11:33:21 +0530

    Update Home.py

--- scripts/Home.py
+++ scripts/Home.py
@@ -57,6 +57,9 @@ st.write("Selected Cohort:", selected_Cohort)
 # Define the GitHub API URLs for the folders containing CSV files
 github_urls = {
     "Career Exploration": 'https://docs.google.com/spreadsheets/d/1nKOWNzHazM49Dw15vMEWhdJEhSTZvk3Wv4DU-eaImHY/edit?usp=sharing',
+    "SWOT Analysis": 'https://docs.google.com/spreadsheets/d/17tiZ7aebyiec8bHgE1vVnFMorBR3pqR870NJXvk7oQ0/edit?usp=sharing',
+    "Searching & Securing Internship": 'https://docs.google.com/spreadsheets/d/1DnUPMOOxqgaPRJ6b0V9e8usGQtkVG1qJ7ffRzl_E5Uc/edit?usp=sharing',
+    "Resume": 'https://docs.google.com/spreadsheets/d/159uBGYMsbJGv70jbXF5vwEdB7m8zbubif_EbY1TFeVg/edit?usp=sharing',
     "SMART Goals": 'https://docs.google.com/spreadsheets/d/19UrJUqaeXPDVcL6_bN0CiIc3fcSh_DputLNl4ciiR3w/edit?usp=sharing',
     "CAP": 'https://docs.google.com/spreadsheets/d/1aEeSsVazN2Zt0gOEOrC8zq5ikE9L9IQ2JA5yP3qUolU/edit?usp=sharing',
     "Linkedin": 'https://docs.google.com/spreadsheets/d/1zp0crVGZn81yrWRJnKNUOjdmPS_hgDJONNsDQKk89A0/edit?usp=sharing'
