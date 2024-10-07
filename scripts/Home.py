2ddbec341261a26343223d41e679e99a1d72b62b
Akshata Satpute <140546785+akshatasatpute@users.noreply.github.com>
2024-10-07 16:39:12 +0530

    Update Home.py

--- scripts/Home.py
+++ scripts/Home.py
@@ -61,9 +61,7 @@ github_urls = {
     "Goal Setting(UK)": 'https://docs.google.com/spreadsheets/d/133hrhht7g2TC8NQIG30JKbSfDSoAr4SkPuuTvcQpYnY/edit?usp=sharing',
     "Resume": 'https://docs.google.com/spreadsheets/d/159uBGYMsbJGv70jbXF5vwEdB7m8zbubif_EbY1TFeVg/edit?usp=sharing',
     "SMART Goals": 'https://docs.google.com/spreadsheets/d/19UrJUqaeXPDVcL6_bN0CiIc3fcSh_DputLNl4ciiR3w/edit?usp=sharing',
-    "Linkedin": 'https://docs.google.com/spreadsheets/d/1zp0crVGZn81yrWRJnKNUOjdmPS_hgDJONNsDQKk89A0/edit?usp=sharing',
     "SMART Goal(UK)":"https://docs.google.com/spreadsheets/d/1fjpfqB-X0hLRzjuIcm8_yquRkFr1Ehr07FLV5UrC5LQ/edit?usp=sharing",
-    "Searching & Securing Internship": 'https://docs.google.com/spreadsheets/d/1DnUPMOOxqgaPRJ6b0V9e8usGQtkVG1qJ7ffRzl_E5Uc/edit?usp=sharing',
     "SWOT Analysis": 'https://docs.google.com/spreadsheets/d/17tiZ7aebyiec8bHgE1vVnFMorBR3pqR870NJXvk7oQ0/edit?usp=sharing'
     
     # Add more assignments as needed
