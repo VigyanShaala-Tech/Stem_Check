e1789785684276abc9bd9d7d387ec4ccadbdb748
Akshata Satpute <140546785+akshatasatpute@users.noreply.github.com>
2024-10-08 18:17:50 +0530

    Update Home.py

--- scripts/Home.py
+++ scripts/Home.py
@@ -56,13 +56,8 @@ st.write("Selected Cohort:", selected_Cohort)
 # Function to read CSV files from a folder path obtained from GitHub and store them in a dictionary
 # Define the GitHub API URLs for the folders containing CSV files
 github_urls = {
-    "Career Exploration": 'https://docs.google.com/spreadsheets/d/1nKOWNzHazM49Dw15vMEWhdJEhSTZvk3Wv4DU-eaImHY/edit?usp=sharing',
-    "CAP": 'https://docs.google.com/spreadsheets/d/1aEeSsVazN2Zt0gOEOrC8zq5ikE9L9IQ2JA5yP3qUolU/edit?usp=sharing',
     "Goal Setting(UK)": 'https://docs.google.com/spreadsheets/d/133hrhht7g2TC8NQIG30JKbSfDSoAr4SkPuuTvcQpYnY/edit?usp=sharing',
-    "Resume": 'https://docs.google.com/spreadsheets/d/159uBGYMsbJGv70jbXF5vwEdB7m8zbubif_EbY1TFeVg/edit?usp=sharing',
-    "SMART Goals": 'https://docs.google.com/spreadsheets/d/19UrJUqaeXPDVcL6_bN0CiIc3fcSh_DputLNl4ciiR3w/edit?usp=sharing',
-    "SMART Goal(UK)":"https://docs.google.com/spreadsheets/d/1fjpfqB-X0hLRzjuIcm8_yquRkFr1Ehr07FLV5UrC5LQ/edit?usp=sharing",
-    "SWOT Analysis": 'https://docs.google.com/spreadsheets/d/17tiZ7aebyiec8bHgE1vVnFMorBR3pqR870NJXvk7oQ0/edit?usp=sharing'
+    "SMART Goal(UK)":"https://docs.google.com/spreadsheets/d/1fjpfqB-X0hLRzjuIcm8_yquRkFr1Ehr07FLV5UrC5LQ/edit?usp=sharing"
     
     # Add more assignments as needed
 }
