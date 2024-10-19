987311f7fba5767535bed3619fd452b83accb792
Akshata Satpute <140546785+akshatasatpute@users.noreply.github.com>
2024-10-19 20:18:49 +0530

    Update Home.py

--- scripts/Home.py
+++ scripts/Home.py
@@ -56,10 +56,8 @@ st.write("Selected Cohort:", selected_Cohort)
 # Function to read CSV files from a folder path obtained from GitHub and store them in a dictionary
 # Define the GitHub API URLs for the folders containing CSV files
 github_urls = {
-    "Goal Setting": 'https://docs.google.com/spreadsheets/d/133hrhht7g2TC8NQIG30JKbSfDSoAr4SkPuuTvcQpYnY/edit?usp=sharing',
     "CV_Resume": "https://docs.google.com/spreadsheets/d/1I0K2vLfm9intnCc34BgdpQl13xLmCiAvNF6jlT2xQFs/edit?usp=sharing",
-    "SMART Goal":"https://docs.google.com/spreadsheets/d/1CpIVi0F98Wa2D3nCnmxs4AMBTjjR3er0ilWWMT4ksGg/edit?usp=sharing",
-    "Career Exploration": "https://docs.google.com/spreadsheets/d/17Is4KMRniuV3yjr9OUYLQKChn4Wkf6lHfwAZmEnb--Q/edit?usp=sharing"
+    "SMART Goal":"https://docs.google.com/spreadsheets/d/1CpIVi0F98Wa2D3nCnmxs4AMBTjjR3er0ilWWMT4ksGg/edit?usp=sharing"
     
     # Add more assignments as needed
 }
